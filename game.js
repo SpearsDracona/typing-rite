30d4a31b9b831c682d24d67a4a44d6371103223f
SpearsDracona <spearsdracona@gmail.com>
2025-05-06 13:53:13 -0400

    Add title screen styling and adjust transition timing

--- game.js
+++ game.js
@@ -7,14 +7,14 @@ function startGame() {
 	void title.offsetWidth;
 	
 	// Fade out title screen
-	title.classList.add('invisible');
 	title.classList.remove('show');
+	title.classList.add('invisible');
 	
 	// Fade in game screen
 	setTimeout(() => {
 		game.classList.remove('invisible');
 		game.classList.add('show');    
-	}, 1000);
+	}, 2000);
 }
 
 // Listen for a keypress
